356d6d27b108d54e5be90d14af3c997e5f89e23a
Juancho09Xt <osorioyepesjuanandres1989@gmail.com>
2023-11-21 20:44:07 -0500

    cree archivo descendete

---
+++ Juan.A/Descendentex.py
@@ -0,0 +1,12 @@
+'''
+Diseñar un algoritmo que me permita imprimir los números del 1 al 10 de forma ascendente y luego los números del 100 al 90 de manera descendente. 
+'''
+
+
+print números del 1 al 10 de forma ascendente
+for i in range(1, 11):
+    print(i)
+
+print números del 100 al 90 de forma descendente
+for j in range(100, 89, -1):
+    print(j)
