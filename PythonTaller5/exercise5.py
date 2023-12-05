e17f53a34b414da5e9714f4b8699fc2eda081ee7
cer03stres <pablo.carmona@correounivalle.edu.co>
2023-12-05 07:31:38 -0500

    punto dado

---
+++ PythonTaller5/exercise5.py
@@ -0,0 +1,37 @@
+import random
+
+class JuegoDado:
+    def __init__(self):
+        self.resultados = []
+        self.lanzamientos = 0
+
+    def lanzamiento_dado(self):
+        #La función randint genera un número entero
+        resultado = random.randint(1, 6)
+        self.resultados.append(resultado)
+        self.lanzamientos += 1
+        return resultado
+
+    def ejecutar_juego(self):
+        while True:
+            input("Presiona Enter para lanzar el dado")
+            resultado = self.lanzamiento_dado()
+
+            print("Resultado del dado:", resultado)
+
+            if resultado == 6:
+                print(f"Felicidades, ¡has sacado un 6 en el lanzamiento número {self.lanzamientos}!")
+                break
+
+            respuesta = input("¿Quieres volver a tirar el dado? presiona S para seguir")
+            if respuesta != 's':
+                break
+
+# Crear una instancia del juego
+juego = JuegoDado()
+
+# Ejecutar el juego
+juego.ejecutar_juego()
+
+# Imprimir los resultados al final
+print("Resultados de los lanzamientos:", juego.resultados)
