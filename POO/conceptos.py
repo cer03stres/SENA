a8a1b6750f80eb6c1ff09662167f5fc84f87b350
cer03stres <pablo.carmona@correounivalle.edu.co>
2023-12-06 07:35:44 -0500

    conceptos

---
+++ POO/conceptos.py
@@ -0,0 +1,12 @@
+'''
+Conceptos POO
+
+POO > Paradigma de programación
+programación orientada a objetos
+
+Clase: Es la plantilla que me permite instanciar objetos
+
+buenas practicas definir las clases con Mayuscula
+
+
+'''
