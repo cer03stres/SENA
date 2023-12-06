5d00ffe528a2d2f11f2024f797da1995edb3cb74
cer03stres <pablo.carmona@correounivalle.edu.co>
2023-12-06 07:52:25 -0500

    conceptos de clases

--- POO/conceptos.py
+++ POO/conceptos.py
@@ -8,5 +8,26 @@ Clase: Es la plantilla que me permite instanciar objetos
 
 buenas practicas definir las clases con Mayuscula
 
+Atributo: son las caractereisticas o propiedades de un objeto
+ej: Color, tamaño, peso, edad
+
+def Lucas():
+ raza = "Pitbull"
+ edad = 2
+ peso = "10kg"
+ vacuna = True
+
+def Candy():
+ raza = "schnauzer"
+ edad  = 2
+ peso  = "5kg"
+ vacuna = False
+
+Objeto: Entidad que posée caracteristicas llamadas
+atributos y tiene acciones llamadas métodos.
+un perro tiene atributos como nombre, edad, etc
+y tiene métodos como ladrar y correr.
 
 '''
+
+
