0266339bede1d1f4dc78c20c3495b8877f8f9715
cer03stres <pablo.carmona@correounivalle.edu.co>
2023-12-06 08:55:53 -0500

    ejercicio coche

--- POO/conceptos.py
+++ POO/conceptos.py
@@ -51,3 +51,33 @@ Class Perro{
 
 
 
+class Coche():
+ #init metodo principal
+ def __init__ (self):
+   self.informacion = {}
+
+
+ def obtener_info(self):
+     self.informacion['marca'] = input("Ingrese la marca")
+     self.informacion['modelo'] = input("Ingrese el modelo")
+
+ def imprimir_datos(self):
+   print("Información del coche")
+   print("Nombre de la Marca:",  self.informacion['marca'])
+   print("Modelo:", self.informacion['modelo'])
+
+ def imprimir_diccionario(self):
+    return self.informacion
+
+#Creacion de objetos
+
+coche = Coche()
+
+imprimir_diccionario = coche.imprimir_diccionario()
+
+coche.obtener_info()
+coche.imprimir_datos()
+
+print("La información del coche es:",  imprimir_diccionario)
+
+#Pas es para que no mande nada todavia
